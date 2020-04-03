e447240a04a352b95c84d43b8c04221ead1dfaee
mast1142 <ssamt1142@gmail.com>
2020-04-03 00:12:20 -0500

    Done.

---
+++ db/migrate/001_create_coupon.rb
@@ -0,0 +1,9 @@
+class CreateCoupon < ActiveRecord::Migration
+    def change
+      create_table :coupon do |t|
+        t.string :coupon_code
+        t.string :store
+      end
+    end
+  end
+  
